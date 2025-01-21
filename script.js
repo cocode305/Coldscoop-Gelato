e7e92b17c4ea9bf4b021a1d3d9494e803f8726a0
xococo <eneluwecc@gmail.com>
2025-01-21 14:06:45 +0100

    Working on the contact page

---
+++ script.js
@@ -0,0 +1,11 @@
+const loginModal = document.querySelector('.login-modal');
+const loginBtn = document.querySelector('.login-link');
+const closeBtn = document.querySelector('.close-btn');
+
+loginBtn.addEventListener('click', () => {
+    loginModal.style.display = "block";
+});
+
+closeBtn.addEventListener('click', (e) => {
+    loginModal.style.display = "none";
+})
