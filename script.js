e0a2e0d2210ec02f20e705f7d63bea48df81213b
xococo <eneluwecc@gmail.com>
2025-03-19 12:38:35 +0100

    Finished working on the mobile version for the index page

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const loginModal = document.querySelector('.login-modal');
 const loginBtn = document.querySelector('.login-link');
 const closeBtn = document.querySelector('.close-btn');
 
+const sideBar = document.querySelector('.sidebar');
+const sideBarOpenBtn = document.querySelector('.open-sidebar')
+const sideBarCloseBtn = document.querySelector('.close-sidebar');
+
+// ================== SIDE BAR SECTION ===============
+sideBarOpenBtn.addEventListener('click',() => {
+    sideBar.style.display = "flex";
+});
+
+sideBarCloseBtn.addEventListener('click',() => {
+    sideBar.style.display = "none";
+});
+
 loginBtn.addEventListener('click', () => {
     loginModal.style.display = "flex";
 });
